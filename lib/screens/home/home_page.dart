a0f57b287bfb2623822373ce16516a6fa75be55a
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2025-09-04 16:19:25 +0100

    Enhance HomePage layout with RichText and improved padding

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_app/theme.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,13 +11,40 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Column(
-        children: [
-          Text('Hello, Fahmi'),
-          SizedBox(height: 5),
-          Text('find the right one for a healthy body'),
-        ],
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: TextSpan(
+                  text: 'Hello ',
+                  style: TextStyle(
+                    color: Color(AppColors.primary),
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: 'Fahmi',
+                      style: TextStyle(
+                        color: Color(AppColors.secondaryVariant),
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 5),
+              Text(
+                'find the right one for\na healthy body'.toUpperCase(),
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
