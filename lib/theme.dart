9d6081baa9a18a64888c6afc184b8fd5bcebe3a4
Toluwalase Bewaji <79145995+Bewizi@users.noreply.github.com>
2025-09-04 16:19:35 +0100

    Fix color definitions in AppColors by adding missing alpha values

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const int primary = 0x3D3014;
-  static const int primaryVariant = 0xF0E9D2;
-  static const int secondary = 0xEEBB4D;
-  static const int secondaryVariant = 0xEEA54D;
-  static const int background = 0xF9F3D0;
+  static const int primary = 0xFF3D3014;
+  static const int primaryVariant = 0xFFF0E9D2;
+  static const int secondary = 0xFFEEBB4D;
+  static const int secondaryVariant = 0xFFEEA54D;
+  static const int background = 0xFFF9F3D0;
   static const int surface = 0xFFFFFFFF;
   static const int error = 0xFFB00020;
   static const int onPrimary = 0xFFFFFFFF;
